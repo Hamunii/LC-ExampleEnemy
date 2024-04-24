ca06c46aa80f65a24b09af5940c5cb14766e01e5
Hamunii <samuel.suni04@gmail.com>
2024-04-24 23:51:34 +0300

    Fix assembly reference to Mod DLL so this actually works

---
+++ UnityProject/Assets/CopySerializedProperties.cs
@@ -0,0 +1,64 @@
+/*
+ * Created by @Kittenji
+ * https://github.com/ChrisFeline
+ */
+
+#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+
+namespace Kittenji.Editor
+{
+    public class CopySerializedProperties
+    {
+        static Object Origin;
+
+        [MenuItem("CONTEXT/Object/Copy Serialized Properties")]
+        static void CopySerialized(MenuCommand command)
+        {
+            Origin = command.context;
+        }
+
+        [MenuItem("CONTEXT/Object/Paste Serialized Properties", validate = true)]
+        static bool ValidateSerialized()
+        {
+            return Origin != null;
+        }
+
+        [MenuItem("CONTEXT/Object/Paste Serialized Properties")]
+        static void PasteSerialized(MenuCommand command)
+        {
+            using (SerializedObject origin = new SerializedObject(Origin))
+            using (SerializedObject target = new SerializedObject(command.context))
+            {
+                origin.Update();
+                target.Update();
+
+                using (SerializedProperty iterator = origin.GetIterator())
+                {
+                    bool visitChildren = true;
+                    iterator.NextVisible(visitChildren);
+                    visitChildren = false;
+
+                    do
+                    {
+                        string name = iterator.name;
+
+                        if (iterator.name == "m_Script")
+                            continue; // Ignore script field
+
+                        using (SerializedProperty property = target.FindProperty(name))
+                        {
+                            if (property != null)
+                                target.CopyFromSerializedPropertyIfDifferent(iterator);
+                        }
+                    }
+                    while (iterator.NextVisible(visitChildren));
+                }
+
+                target.ApplyModifiedProperties();
+            }
+        }
+    }
+}
+#endif
